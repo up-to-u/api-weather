cb8dca72da542e866f4c400e7ed08ad8bc505362
Arisa <arisadev@gmail.com>
2022-09-07 09:56:25 +0700

    [UPDATE]

---
+++ style-font_497X528.php
@@ -0,0 +1,171 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@300;400&display=swap" rel="stylesheet">
+<style>
+    body {
+        font-family: 'Noto Sans Thai', sans-serif;
+    }
+
+    .object-wrapper {
+        display: flex;
+        justify-content: left;
+        align-items: left;
+        min-height: 95vh;
+    }
+
+    /* API LINE 1 */
+    .TXT-HEADER {
+        margin: 20px;
+        font-size: 30px;
+        font-family: 'Noto Sans Thai', sans-serif;
+    }
+
+    .ICON-IMG-MAIN1 {
+        position: absolute;
+        width: 125px;
+        margin-left: -59px;
+        margin-top: -50px;
+    }
+
+    .ICON-IMG-MAIN2 {
+        position: absolute;
+        width: 125px;
+        margin-left: -63px;
+        margin-top: -50px;
+    }
+
+    .ICON-IMG-MAIN3 {
+        position: absolute;
+        width: 130px;
+        margin-left: -59px;
+        margin-top: -40px;
+    }
+
+    .ICON-IMG-MAIN4 {
+        position: absolute;
+        width: 110px;
+        margin-left: -57px;
+        margin-top: -50px;
+    }
+
+    .ICON-IMG-MAIN5 {
+        position: absolute;
+        width: 110px;
+        margin-left: -55px;
+        margin-top: -50px;
+    }
+
+    .TEMP-MAIN {
+        font-size: 77px;
+        margin-top: 38px;
+        line-height: 60px;
+        font-weight: 900;
+    }
+
+    .cycles-main {
+        width: 20px;
+        margin-left: 38px;
+        margin-top: 15px;
+    }
+
+    .TXT_F1 {
+        margin-top: 20px;
+        font-size: 46px;
+    }
+
+    .TXT_F2 {
+        font-size: 35px;
+    }
+
+    .TXT_F3MAX {
+        font-size: 32px;
+        font-weight: 1000;
+    }
+
+    .TXT_F3MIN {
+        font-size: 32px;
+        font-weight: 10;
+
+    }
+
+    .TXT_F4 {
+        font-size: 50px;
+        margin-top: -7px;
+    }
+
+    .cycles {
+        width: 13px;
+        margin-left: 20px;
+    }
+
+    /* API LINE 2 */
+    .TXT_DAYS-FIRST {
+        margin-top: 5px;
+        font-size: 43px;
+        color: #FFFFFF;
+    }
+
+    .TXT_DAYS {
+        margin-top: 5px;
+        font-size: 43px;
+    }
+
+    .IMG-DAY-ICON {
+        margin-top: 15px;
+        width: 80px;
+
+    }
+
+    .IMG-DAY-ICON1 {
+        margin-top: -5px;
+        width: 60px;
+
+    }
+
+    .IMG-DAY-ICON2 {
+        margin-top: -5px;
+        width: 60px;
+
+    }
+
+    .IMG-DAY-ICON3 {
+        margin-top: -10px;
+        width: 60px;
+
+    }
+
+    .IMG-DAY-ICON4 {
+        margin-top: 0px;
+        width: 60px;
+
+    }
+
+    .IMG-DAY-ICON5 {
+        margin-top: 0px;
+        width: 60px;
+
+    }
+
+    .TXT_RAIN_VALUE_FIRST {
+        color: #FFFFFF;
+        margin-top: 8px;
+        font-size: 25px;
+        font-weight: 600;
+    }
+
+    .TXT_RAIN_VALUE {
+        color: #5AA1D9;
+        margin-top: 8px;
+        font-size: 25px;
+        font-weight: 900;
+    }
+
+    .vl {
+        border-left: 1px solid white;
+        height: 150px;
+        align-items:center;
+        margin-top: 0px;
+        margin-left: 7px;
+        /* display:inline; */
+    }
+</style>
